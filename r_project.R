c54c31c416e01e60cf4cfb5a373e586c40423a9b
enemcik <81751196@fsv.cuni.cz>
2020-05-05 16:26:06 +0200

    Adding errors and aucs.

--- r_project.R
+++ r_project.R
@@ -166,7 +166,7 @@ rf = randomForest(as.factor(target)~ï..age+sex+cp+trestbps+chol+fbs+restecg+thal
                   +exang+oldpeak+slope+ca+thal, data = train_data,ntree = 33, nodesize =7, importance = T, proximity = TRUE)
 
 pred_rf = predict(rf, val_data)
-val_data$pred_rf = as.numeric(pred_rf)
+val_data$pred_rf = pred_rf
 confusionMatrix(table(val_data$target,val_data$pred_rf), positive = "1")
 
 auc_rf = auc(val_data$target,val_data$pred_rf) #auc
@@ -184,7 +184,18 @@ acc_log = NULL
 acc_cart = NULL
 acc_nn = NULL
 acc_xgboost = NULL
-acc_rf = NULL
+acc_rf = confusionMatrix(table(val_data$target,val_data$pred_rf), positive = "1")$overall['Accuracy']
+er_log = NULL
+er_cart = NULL
+er_nn = NULL
+er_xgboost = NULL
+er_rf = mean(pred_rf != val_data$target)
+ac_log = NULL
+ac_cart = NULL
+ac_nn = NULL
+ac_xgboost = NULL
+ac_rf = auc(val_data$target,as.numeric(val_data$pred_rf))
+
 set.seed(100)
 
 for (i in 1:k){ #sample randomly 100 times
@@ -200,6 +211,8 @@ for (i in 1:k){ #sample randomly 100 times
     prob_log = predict(logit, val_data) #prediction
     pred_log = as.numeric(prob_log > 0.5)
     acc_log[i] = confusionMatrix(table(val_data$target,pred_log), positive = "1")$overall['Accuracy']
+    er_log[i] = mean(pred_log != val_data$target) #error
+    auc_log[i] = auc(val_data$target,val_data$pred_log)
   }, TRUE)
   ##DECISION TREE
   cart = rpart(fmla, data = train_data, method = 'class')
@@ -207,6 +220,8 @@ for (i in 1:k){ #sample randomly 100 times
     prob_cart = predict(cart, val_data) #prediction
     pred_cart = as.numeric(prob_cart[,2] > 0.5)
     acc_cart[i] = confusionMatrix(table(val_data$target,pred_cart), positive = "1")$overall['Accuracy']
+    er_cart[i] = mean(pred_cart != val_data$target) #error
+    auc_cart[i] = auc(val_data$target,val_data$pred_cart)
   },TRUE)
   ##NEURAL NET
   nn = nnet(target~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
@@ -215,9 +230,40 @@ for (i in 1:k){ #sample randomly 100 times
     prob_nn = predict(nn, val_data)
     pred_nn = as.numeric(prob_nn > 0.5)
     acc_nn[i] = confusionMatrix(table(val_data$target,pred_nn), positive = "1")$overall['Accuracy']
-  })
+    er_nn[i] = mean(pred_nn != val_data$target) #error
+    auc_nn[i] = auc(val_data$target,val_data$pred_nn)
+  },TRUE)
+  ##BOOSTED TREE
+  sparse_matrix = sparse.model.matrix(target ~ ., data = train_data)[,-1] #dummy contrast coding of categorical variables to fit the xgboost
+  sparse_matrix_val = sparse.model.matrix(target ~ ., data = val_data[,colnames(train_data)])[,-1]
+    
+  labels = train_data$target
+    
+  bst = xgboost(data = sparse_matrix, label = labels, max_depth = 4,
+                  eta = 1, nthread = 2, nrounds = 10,objective = "binary:logistic")
+  try({
+    prob_xgboost = predict(bst, sparse_matrix_val)
+    pred_xgboost = as.numeric(prob_xgboost > 0.5)
+    acc_xgboost[i] = confusionMatrix(table(val_data$target,pred_xgboost), positive = "1")$overall['Accuracy']
+    er_xgboost[i] = mean(pred_xgboost != val_data$target) #error
+    auc_xgboost[i] = auc(val_data$target,val_data$pred_xgboost)
+  }, TRUE)
 }
 
 print(mean(acc_log, na.rm = TRUE))
 print(mean(acc_cart, na.rm = TRUE))
 print(mean(acc_nn, na.rm = TRUE))
+print(mean(acc_xgboost, na.rm = TRUE))
+print(acc_rf)
+
+print(mean(er_log, na.rm = TRUE))
+print(mean(er_cart, na.rm = TRUE))
+print(mean(er_nn, na.rm = TRUE))
+print(mean(er_xgboost, na.rm = TRUE))
+print(er_rf)
+
+print(mean(auc_log, na.rm = TRUE))
+print(mean(auc_cart, na.rm = TRUE))
+print(mean(auc_nn, na.rm = TRUE))
+print(mean(auc_xgboost, na.rm = TRUE))
+print(auc_rf)
