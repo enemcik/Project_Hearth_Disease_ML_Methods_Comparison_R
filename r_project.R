15266e1854892aa0bfe3a3d1e802144bbf4f11cc
enemcik <81751196@fsv.cuni.cz>
2020-05-13 14:52:53 +0200

    Correcting levels, adding finetuning for nnet.

--- r_project.R
+++ r_project.R
@@ -46,20 +46,27 @@ data$slope = as.factor(data$slope) #slope
 data$ca = as.factor(data$ca) #number of colored vessels
 data$thal = as.factor(data$thal) #thal
 
+data = data[data$thal!=0,]  #removing and merging very rare levels /not informative
+data$thal = factor(data$thal)
+data$ca[data$ca==4] = 3
+data$ca = factor(data$ca)
+
 
 str(data) #variables
 summary(data) #variable statistics
 sapply(data, sd)
 apply(data, 2, function(x) {sum(is.na(x))}) #NAs inspection, 0 present
+summary(data)
+
 data_cv = data #cross-validation
 
 summary(data_cv$target)
 
-set.seed(900) #for reproducibility
-
 n = nrow(data) #shuffling the data
+set.seed(104)
 data = data[sample(n),] 
 
+set.seed(46)
 rand_rows = sample(1:nrow(data), 0.6*nrow(data)) #split into training and validation dataset
 train_data = data[rand_rows, ]
 val_data = data[-rand_rows, ]
@@ -75,24 +82,13 @@ fmla
 
 k = 100
 acc_log = NULL
-acc_nn = NULL
-acc_xgboost = NULL
 er_log = NULL
-er_cart = NULL
-er_nn = NULL
-er_xgboost = NULL
 auc_log = NULL
-auc_nn = NULL
-auc_xgboost = NULL
 cut_log = NULL
-cut_nn = NULL
-cut_xgboost = NULL
-cut_rf = NULL
-
-set.seed(100)
 
 for (i in 1:k){ #sample randomly 100 times
   n = nrow(data_cv)
+  set.seed(21)
   data_temp = data_cv[sample(n),] 
   rand_rows = sample(1:nrow(data_temp), 0.7*nrow(data_temp)) #split into training and validation dataset
   train_data = data_temp[rand_rows, ]
@@ -119,34 +115,12 @@ for (i in 1:k){ #sample randomly 100 times
     pred_log = as.numeric(prob_log > cutoff_log) #ERROR- minimized by maximizing ACCURACY using its cutoff
     er_log[i] = mean(pred_log != val_data$target)
   },TRUE)
-  
-  ##NEURAL NET
-  nn = nnet(target~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
-            +exang+oldpeak+slope+ca+thal, data = train_data, size = 5, decay = 5e-4, maxit = 100)
-  try({
-    prob_nn = predict(nn, val_data)
-    pred_nn = ROCR::prediction(prob_nn, val_data$target) #create a prediction object with true values and predicted ones
-    
-    roc.perf_nn = ROCR::performance(pred_nn, measure = "tpr", x.measure = "fpr") #performance measure as ROC
-    
-    auc.perf_nn = ROCR::performance(pred_nn, measure = "auc") #performance measure AUC
-    auc_nn[i] = auc.perf_nn@y.values[[1]]
-    
-    acc.perf_nn = ROCR::performance(pred_nn, measure = "acc") # find best cutoff according to accuracy
-    
-    ind_nn = which.max( slot(acc.perf_nn, "y.values")[[1]] )
-    acc_nn[i] = slot(acc.perf_nn, "y.values")[[1]][ind_nn]
-    cutoff_nn = slot(acc.perf_nn, "x.values")[[1]][ind_nn]
-    cut_nn[i] = cutoff_nn
-    
-    pred_nn = as.numeric(prob_nn > cutoff_nn) #ERROR- minimized by maximizing ACCURACY using its cutoff
-    er_nn[i] = mean(pred_nn != val_data$target)
-  },TRUE)
 }
 
 ##DECISION TREE - BAGGED ##DONE
 m<-dim(train_data)[1]
 ntree<-500
+set.seed(65)
 samples<-sapply(1:ntree,
                 FUN = function(iter)
                 {sample(1:m, size=m, replace=T)}) #replace=T makes it a bootstrap 
@@ -190,6 +164,7 @@ try({
 #er_cart = mean(abs(prob_bagtree - val_data$target)) - probability error 
 
 ###RANDOM FOREST ##DONE
+set.seed(87)
 rf = randomForest(as.factor(target)~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
                   +exang+oldpeak+slope+ca+thal, nodesize = 1 ,data = train_data, proximity = TRUE)
 
@@ -214,8 +189,9 @@ cutoff_rf = slot(acc.perf_rf, "x.values")[[1]][ind_rf]
 cut_rf = cutoff_rf
 print(c(accuracy= acc_rf, cutoff = cutoff_rf))
 
-pred_rf = as.numeric(prob_rf > cutoff_rf) #ERROR- minimized by maximizing ACCURACY using its cutoff
+pred_rf = as.numeric(prob_rf[,2] > cutoff_rf) #ERROR- minimized by maximizing ACCURACY using its cutoff
 er_rf = mean(pred_rf != val_data$target)
+
 
 ###XGBOOST### ##DONE
 sparse_matrix = sparse.model.matrix(target ~ ., data = train_data)[,-1] #dummy contrast coding of categorical variables to fit the xgboost
@@ -223,19 +199,6 @@ sparse_matrix_val = sparse.model.matrix(target ~ ., data = val_data[,colnames(tr
 
 labels = as.numeric(train_data$target)
 ts_label = as.numeric(val_data$target)
-
-dtrain = xgb.DMatrix(data=sparse_matrix, label = labels)
-dval = xgb.DMatrix(data = sparse_matrix_val, label = ts_label)
-
-params = list(booster = "gbtree", objective = "binary:logistic",
-              eta = 0.3, gamma = 0, max_depth = 6, min_child_weight = 1,
-              subsample = 1, colsample_bytree = 1)
-
-xgbcv = xgb.cv(params = params, data = dtrain, nrounds = 100, nfold = 5, #11 lowest
-               showsd = 5, stratified = T, print_every_n = 1,
-               early_stoppping_rounds = 20, maximize = F)
-
-min(xgbcv$evaluation_log$test_error_mean)
 
 fact_col = colnames(train_data)[sapply(train_data,is.character)]
 for(i in fact_col) set (train_data, j = i, value = factor(data_train[i]))
@@ -260,14 +223,19 @@ params = makeParamSet(makeDiscreteParam("booster",
                       makeNumericParam("gamma",lower = 0, upper = 2),
                       makeNumericParam("eta", lower = 0.1, upper = 3))
 rdesc = makeResampleDesc("CV", stratify = T, iters = 5L)
-ctrl = makeTuneControlRandom(maxit = 200L)
+ctrl = makeTuneControlRandom(maxit = 100L)
+#ctrl = makeTuneControlGrid(resolution = c(max_depth = 10, nrounds = 10, min_child_weight = 50,
+#                                          subsample = 50, colsample_bytree = 50, gamma = 50,
+#                                          eta = 50), tune.threshold = FALSE)
 
 parallelStartSocket(cpus=detectCores())
+set.seed(121)
 mytune = tuneParams(learner = lrn, task = traintask, resampling = rdesc,
                     measures = acc, par.set = params, control = ctrl,
                     show.info = F)
 
 lrn_tune = setHyperPars(lrn, par.vals = mytune$x)
+set.seed(11)
 xgmodel = train(learner = lrn_tune, task = traintask)
 xgpred = predict(xgmodel,testtask)
 acc_xgboost = confusionMatrix(xgpred$data$response,xgpred$data$truth)$overall[1]
@@ -277,29 +245,41 @@ auc = generateThreshVsPerfData(xgpred, measures = list(fpr, tpr, mmce))
 plotROCCurves(auc)
 auc_xgboost = mlr::performance(xgpred, mlr::auc)
 
-print(mean(acc_log, na.rm = TRUE))
-print(acc_cart)
-print(mean(acc_nn, na.rm = TRUE))
-print(acc_xgboost)
-print(acc_rf)
 
-print(mean(er_log, na.rm = TRUE))
-print(er_cart)
-print(mean(er_nn, na.rm = TRUE))
-print(er_xgboost)
-print(er_rf)
+##NNET
+traintask = makeClassifTask(data = train_data, target = "target")
+testtask = makeClassifTask(data = val_data, target = "target")
 
-print(mean(auc_log, na.rm = TRUE))
-print(auc_cart)
-print(mean(auc_nn, na.rm = TRUE))
-print(auc_xgboost)
-print(auc_rf)
+traintask = createDummyFeatures(obj = traintask)
+testtask = createDummyFeatures(obj = testtask)
 
-print(mean(cut_log, na.rm = TRUE))
-print(cut_cart)
-print(mean(cut_nn, na.rm = TRUE))
-#print(mean(cut_xgboost, na.rm = TRUE))
-print(cut_rf)
+lrn = makeLearner("classif.nnet",predict.type = "prob")
+lrn$par.vals = list(maxit = 100, na.action = na.omit)
+
+params = makeParamSet(
+                      makeIntegerParam("size", lower = 2L, upper = 10L),
+                      makeNumericParam("decay", lower = -3, upper = 3))
+rdesc = makeResampleDesc("CV", stratify = T, iters = 5L)
+#design = generateGridDesign(params, resolution = c(size = 10, decay = 100))
+ctrl = makeTuneControlRandom(maxit = 100L)
+#ctrl = makeTuneControlGrid(resolution = c(size = 10, decay = 500), tune.threshold = FALSE)
+set.seed(49)
+mytune = tuneParams(learner = lrn, task = traintask, resampling = rdesc,
+                    measures = acc, par.set = params, control = ctrl,
+                    show.info = F)
+mytune$y
+
+lrn_tune = setHyperPars(lrn, par.vals = mytune$x)
+set.seed(95)
+nnetmodel = train(learner = lrn_tune, task = traintask)
+nnetpred = predict(nnetmodel,testtask)
+acc_nn = confusionMatrix(nnetpred$data$response,nnetpred$data$truth)$overall[1]
+er_nn = mean(nnetpred$data$response != nnetpred$data$truth)
+
+auc = generateThreshVsPerfData(nnetpred, measures = list(fpr, tpr, mmce))
+plotROCCurves(auc)
+auc_nn = mlr::performance(nnetpred, mlr::auc)
+
 
 #######################
 ####Replicated Paper###
@@ -307,7 +287,6 @@ print(cut_rf)
 
 ###LOGISTIC REGRESSION### ##DONE
 logit = glm(fmla, data = train_data, family = "binomial")
-summary(logit)
 
 prob_log = predict(logit, val_data, type = "response")
 pred_log = ROCR::prediction(prob_log, val_data$target) #create a prediction object with true values and predicted ones
@@ -316,7 +295,7 @@ roc.perf_log = ROCR::performance(pred_log, measure = "tpr", x.measure = "fpr") #
 plot(roc.perf_log)
 
 auc.perf_log = ROCR::performance(pred_log, measure = "auc") #performance measure AUC
-auc.perf_log@y.values
+auc_log_old = auc.perf_log@y.values[[1]]
 
 opt.cut = function(perf, pred){
   cut.ind = mapply(FUN=function(x, y, p){
@@ -332,17 +311,16 @@ acc.perf_log = ROCR::performance(pred_log, measure = "acc") # find best cutoff a
 plot(acc.perf_log)
 
 ind_log = which.max( slot(acc.perf_log, "y.values")[[1]] )
-acc_log = slot(acc.perf_log, "y.values")[[1]][ind_log]
+acc_log_old = slot(acc.perf_log, "y.values")[[1]][ind_log]
 cutoff_log = slot(acc.perf_log, "x.values")[[1]][ind_log]
-print(c(accuracy= acc_log, cutoff = cutoff_log))
+print(c(accuracy= acc_log_old, cutoff = cutoff_log))
 
 pred_log = as.numeric(prob_log > cutoff_log) #ERROR- minimized by maximizing ACCURACY using its cutoff
-err_log = mean(pred_log != val_data$target)
-err_log
+er_log_old = mean(pred_log != val_data$target)
+er_log_old
 
 ###DECISION TREES### ##DONE
 cart = rpart(fmla, data = train_data, method = 'class')
-summary(cart)
 rpart.plot(cart)
 
 prob_cart = predict(cart, val_data) #prediction
@@ -353,25 +331,25 @@ roc.perf_cart = ROCR::performance(pred_cart, measure = "tpr", x.measure = "fpr")
 plot(roc.perf_cart)
 
 auc.perf_cart = ROCR::performance(pred_cart, measure = "auc") #performance measure AUC
-auc.perf_cart@y.values
+auc_cart_old = auc.perf_cart@y.values[[1]]
 print(opt.cut(roc.perf_cart, pred_cart))
 
 acc.perf_cart = ROCR::performance(pred_cart, measure = "acc") # find best cutoff according to accuracy
 plot(acc.perf_cart)
 
 ind_cart = which.max( slot(acc.perf_cart, "y.values")[[1]] )
-acc_cart = slot(acc.perf_cart, "y.values")[[1]][ind_cart]
+acc_cart_old = slot(acc.perf_cart, "y.values")[[1]][ind_cart]
 cutoff_cart = slot(acc.perf_cart, "x.values")[[1]][ind_cart]
-print(c(accuracy= acc_cart, cutoff = cutoff_cart))
+print(c(accuracy= acc_cart_old, cutoff = cutoff_cart))
 
-pred_cart = as.numeric(prob_cart > cutoff_cart) #ERROR- minimized by maximizing ACCURACY using its cutoff
-err_cart = mean(pred_cart != val_data$target)
-err_cart
+pred_cart = as.numeric(prob_cart[,2] > cutoff_cart) #ERROR- minimized by maximizing ACCURACY using its cutoff
+er_cart_old = mean(pred_cart != val_data$target)
+er_cart_old
 
 ###NEURAL NETWORKS### ##DONE
+set.seed(69)
 nn = nnet(target~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
           +exang+oldpeak+slope+ca+thal, data = train_data, size = 5, decay = 5e-4, maxit = 100)
-summary(nn)
 ##code for optimal cutoffs and etc for logit seems to work here 
 prob_nn = predict(nn, val_data)
 pred_nn = ROCR::prediction(prob_nn, val_data$target) #create a prediction object with true values and predicted ones
@@ -380,7 +358,7 @@ roc.perf_nn = ROCR::performance(pred_nn, measure = "tpr", x.measure = "fpr") #pe
 plot(roc.perf_nn)
 
 auc.perf_nn = ROCR::performance(pred_nn, measure = "auc") #performance measure AUC
-auc.perf_nn@y.values
+auc_nn_old = auc.perf_nn@y.values[[1]]
 
 print(opt.cut(roc.perf_nn, pred_nn)) #formula which finds "optimal" cutoff weighting both sensitivity and specificity equally (TPR and FPR)
 
@@ -388,10 +366,50 @@ acc.perf_nn = ROCR::performance(pred_nn, measure = "acc") # find best cutoff acc
 plot(acc.perf_nn)
 
 ind_nn = which.max( slot(acc.perf_nn, "y.values")[[1]] )
-acc_nn = slot(acc.perf_nn, "y.values")[[1]][ind_nn]
+acc_nn_old = slot(acc.perf_nn, "y.values")[[1]][ind_nn]
 cutoff_nn = slot(acc.perf_nn, "x.values")[[1]][ind_nn]
-print(c(accuracy= acc_nn, cutoff = cutoff_nn))
+print(c(accuracy= acc_nn_old, cutoff = cutoff_nn))
 
 pred_nn = as.numeric(prob_nn > cutoff_nn) #ERROR- minimized by maximizing ACCURACY using its cutoff
-err_nn = mean(pred_nn != val_data$target)
-err_nn
+er_nn_old = mean(pred_nn != val_data$target)
+er_nn_old
+
+################
+###STATISTICS###
+################
+
+###NEW###
+print(mean(acc_log, na.rm = TRUE))
+print(acc_cart)
+print(acc_nn)
+print(acc_xgboost)
+print(acc_rf)
+
+print(mean(er_log, na.rm = TRUE))
+print(er_cart)
+print(er_nn)
+print(er_xgboost)
+print(er_rf)
+
+print(mean(auc_log, na.rm = TRUE))
+print(auc_cart)
+print(auc_nn)
+print(auc_xgboost)
+print(auc_rf)
+
+print(mean(cut_log, na.rm = TRUE))
+print(cut_cart)
+print(cut_rf)
+
+###OLD###
+print(acc_log_old)
+print(acc_cart_old)
+print(acc_nn_old)
+
+print(er_log_old)
+print(er_cart_old)
+print(er_nn_old)
+
+print(auc_log_old)
+print(auc_cart_old)
+print(auc_nn_old)
