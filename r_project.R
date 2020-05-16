c28381b283405de06b81ea2fd9e7da06f83ddb9d
Paul Mainka <75650712@fsv.cuni.cz>
2020-05-16 18:19:55 +0200

    Bagged trees fix and and reorganization of the code

--- r_project.R
+++ r_project.R
@@ -76,6 +76,122 @@ y = "target"
 fmla = paste(y, paste(vars, collapse="+"),sep="~")
 fmla
 
+#### creating 2 functions which we will use ###
+opt.cut = function(perf, pred){ # function which finds optimal cutoff level maximizing a trade-off between Sensitivity& Specifivity
+  cut.ind = mapply(FUN=function(x, y, p){
+    d = (x - 0)^2 + (y-1)^2
+    ind = which(d == min(d))
+    c(sensitivity = y[[ind]], specificity = 1-x[[ind]], 
+      cutoff = p[[ind]])
+  }, perf@x.values, perf@y.values, pred@cutoffs)
+}
+performanceMeasures<-function(prob, truth, name="model") { #Function for various accuracy measures we use.
+  pred= ROCR::prediction(prob, truth)
+  roc = ROCR::performance(pred, measure = "tpr", x.measure = "fpr") #performance measure as ROC
+  auc.perf= ROCR::performance(pred, measure = "auc") #performance measure AUC
+  auc= auc.perf@y.values[[1]]
+  sen_spe = opt.cut(roc, pred)
+  acc.perf = ROCR::performance(pred, measure = "acc") # find best cutoff according to accuracy
+  ind = which.max( slot(acc.perf, "y.values")[[1]] )
+  acc = slot(acc.perf, "y.values")[[1]][ind]
+  cutoff= slot(acc.perf, "x.values")[[1]][ind]
+  pred_cut = as.numeric(prob > cutoff) #ERROR- minimized by maximizing ACCURACY using its cutoff
+  er = mean(pred_cut != truth)
+  data.frame(model=name, Area_under_the_curve = auc, Sensitivity = sen_spe[1], 
+             Specificity= sen_spe[2], Cutoff_Sen_Spe= sen_spe[3], Accuracy=acc, Cutoff_acc = cutoff, error=er)
+}
+#######################
+####Replicated Paper###
+#######################
+
+###LOGISTIC REGRESSION### ##DONE
+logit = glm(fmla, data = train_data, family = "binomial")
+
+prob_log_train = predict(logit, train_data, type = "response")
+prob_log_test = predict(logit, val_data, type = "response")
+
+perf_log_train = performanceMeasures(prob_log_train, train_data$target, name="Logit Training")
+perf_log_train
+perf_log_test = performanceMeasures(prob_log_test, val_data$target, name="Logit Testing")
+
+###DECISION TREES### ##DONE
+cart = rpart(fmla, data = train_data, method = 'class')
+rpart.plot(cart)
+
+prob_cart_train = predict(cart, train_data)
+prob_cart_test = predict(cart, val_data) #prediction
+
+perf_cart_train = performanceMeasures(prob_cart_train[,2], train_data$target, name="DECISION TREES Training")
+perf_cart_train
+perf_cart_test = performanceMeasures(prob_cart_test[,2], val_data$target, name="DECISION TREES Testing")
+perf_cart_test
+
+###NEURAL NETWORKS### ##DONE
+set.seed(69)
+nn = nnet(target~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
+          +exang+oldpeak+slope+ca+thal, data = train_data, size = 5, decay = 5e-4, maxit = 100)
+
+prob_nn_train = predict(nn, train_data)
+prob_nn_test = predict(nn, val_data) #prediction
+
+perf_nn_train = performanceMeasures(prob_nn_train, train_data$target, name="NEURAL NETWORKS Training")
+perf_nn_train
+perf_nn_test = performanceMeasures(prob_nn_test, val_data$target, name="NEURAL NETWORKS Testing")
+perf_nn_test
+
+###########################
+#### Additional methods####
+###########################
+
+##DECISION TREE - BAGGED ##DONE 
+m<-dim(train_data)[1]
+ntree<-500
+set.seed(65)
+samples<-sapply(1:ntree,
+                FUN = function(iter)
+                {sample(1:m, size=m, replace=T)}) #replace=T makes it a bootstrap 
+
+treelist<-lapply(1:ntree, #Training the individual decision trees and return them in a list
+                 FUN=function(iter) {
+                   samp <- samples[,iter];
+                   rpart(fmla,train_data[samp,], method= "class")
+                 }
+)
+#predict.bag assumes the underlying classifier returns decision probabilities, not decisions. 
+predict.bag<-function(treelist,newdata) {
+  preds<-lapply(1:length(treelist),
+                FUN=function(iter) {
+                  predict(treelist[[iter]],newdata=newdata)
+                }
+  )
+  preds
+}
+prob_bagtree_train = predict.bag(treelist, newdata=train_data)
+prob_bagtree_train= as.data.frame(prob_bagtree_train)
+prob_bagtree_train_1= rowSums(prob_bagtree_train[, seq(2, ncol(prob_bagtree_train), 2)])/length(treelist)
+
+prob_bagtree_test = predict.bag(treelist, newdata=val_data)
+prob_bagtree_test= as.data.frame(prob_bagtree_test)
+prob_bagtree_test_1= rowSums(prob_bagtree_test[, seq(2, ncol(prob_bagtree_test), 2)])/length(treelist)
+
+perf_bagtree_train = performanceMeasures(prob_bagtree_train_1, train_data$target, name="Bagged Trees Training")
+perf_bagtree_train
+perf_bagtree_test = performanceMeasures(prob_bagtree_test_1, val_data$target, name="Bagged Trees Testing")
+perf_bagtree_test
+
+###RANDOM FOREST ##DONE
+set.seed(87)
+rf = randomForest(as.factor(target)~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
+                  +exang+oldpeak+slope+ca+thal, nodesize = 1 ,data = train_data, proximity = TRUE)
+
+prob_rf_train = predict(rf, train_data, type='prob') # returns probabilities not 0,1 values with type="prob"
+prob_rf_test = predict(rf, val_data, type='prob')
+
+perf_rf_train = performanceMeasures(prob_rf_train[,2], train_data$target, name="RANDOM FOREST Training")
+perf_rf_train
+perf_rf_test = performanceMeasures(prob_rf_test[,2], val_data$target, name="RANDOM FOREST Testing")
+perf_rf_test
+
 ######################
 ####Cross-validated###
 ######################
@@ -93,10 +209,14 @@ prob_nn_train= NULL
 prob_nn_test= NULL
 perf_nn_train= as.data.frame( matrix(0, ncol = 0, nrow = 0))
 perf_nn_test=as.data.frame( matrix(0, ncol = 0, nrow = 0))
-prob_rftrain= NULL
-prob_rftest= NULL
+prob_rf_train= NULL
+prob_rf_test= NULL
 perf_rf_train= as.data.frame( matrix(0, ncol = 0, nrow = 0))
 perf_rf_test=as.data.frame( matrix(0, ncol = 0, nrow = 0))
+prob_bagtree_train= NULL
+prob_bagtree_test= NULL
+perf_bagtree_train= as.data.frame( matrix(0, ncol = 0, nrow = 0))
+perf_bagtree_test=as.data.frame( matrix(0, ncol = 0, nrow = 0))
 
 for (i in 1:k){ #sample randomly 100 times
   n = nrow(data_cv)
@@ -135,8 +255,29 @@ for (i in 1:k){ #sample randomly 100 times
     prob_rf_test = predict(rf, val_data, type='prob')
     perf_rf_train = rbind(perf_rf_train,performanceMeasures(prob_rf_train[,2], train_data$target, name="RANDOM FOREST Training"))
     perf_rf_test = rbind(perf_rf_test,performanceMeasures(prob_rf_test[,2], val_data$target, name="RANDOM FOREST Testing"))
-    })
- 
+    },TRUE)
+  
+  samples<-sapply(1:ntree,
+                  FUN = function(iter)
+                  {sample(1:m, size=m, replace=T)}) #replace=T makes it a bootstrap 
+  treelist<-lapply(1:ntree, #Training the individual decision trees and return them in a list
+                     FUN=function(iter) {
+                       samp <- samples[,iter];
+                       rpart(fmla,train_data[samp,], method= "class")
+                     }
+    )
+  try({
+    prob_bagtree_train = predict.bag(treelist, newdata=train_data)
+    prob_bagtree_train= as.data.frame(prob_bagtree_train)
+    prob_bagtree_train_1= rowSums(prob_bagtree_train[, seq(2, ncol(prob_bagtree_train), 2)])/length(treelist)
+    
+    prob_bagtree_test = predict.bag(treelist, newdata=val_data)
+    prob_bagtree_test= as.data.frame(prob_bagtree_test)
+    prob_bagtree_test_1= rowSums(prob_bagtree_test[, seq(2, ncol(prob_bagtree_test), 2)])/length(treelist)
+    
+    perf_bagtree_train = rbind(perf_bagtree_train, performanceMeasures(prob_bagtree_train_1, train_data$target, name="Bagged Trees Training"))
+    perf_bagtree_test = rbind(perf_bagtree_test, performanceMeasures(prob_bagtree_test_1, val_data$target, name="Bagged Trees Testing"))
+    },TRUE)
 }
 
 summary(perf_log_train)
@@ -151,64 +292,9 @@ summary(perf_nn_test)
 summary(perf_rf_train)
 summary(perf_rf_test)
 
-##DECISION TREE - BAGGED ##DONE ### Doesnt work (at least the performance measures)
-m<-dim(train_data)[1]
-ntree<-500
-set.seed(65)
-samples<-sapply(1:ntree,
-                FUN = function(iter)
-                {sample(1:m, size=m, replace=T)}) #replace=T makes it a bootstrap 
-try({
-  treelist<-lapply(1:ntree, #Training the individual decision trees and return them in a list
-                   FUN=function(iter) {
-                     samp <- samples[,iter];
-                     rpart(fmla,train_data[samp,])
-                   }
-  )
-  #predict.bag assumes the underlying classifier returns decision probabilities, not decisions. 
-  predict.bag<-function(treelist,newdata) {
-    preds<-sapply(1:length(treelist),
-                  FUN=function(iter) {
-                    predict(treelist[[iter]],newdata=newdata)
-                  }
-    )
-    predsums<-rowSums(preds)
-    predsums/length(treelist)
-  }
-  
-  prob_bagtree = predict.bag(treelist, newdata=val_data)
-  pred_bagtree = ROCR::prediction(prob_bagtree, val_data$target)
-  
-  roc.perf_bagtree = ROCR::performance(pred_bagtree, measure = "tpr", x.measure = "fpr") #performance measure as ROC
-  
-  auc.perf_bagtree = ROCR::performance(pred_bagtree, measure = "auc") #performance measure AUC
-  auc_cart = auc.perf_bagtree@y.values[[1]]
-  
-  acc.perf_bagtree = ROCR::performance(pred_bagtree, measure = "acc") # find best cutoff according to accuracy
-  
-  ind_bagtree = which.max( slot(acc.perf_bagtree, "y.values")[[1]] )
-  acc_cart = slot(acc.perf_bagtree, "y.values")[[1]][ind_bagtree]
-  cutoff_bagtree = slot(acc.perf_bagtree, "x.values")[[1]][ind_bagtree]
-  cut_cart = cutoff_bagtree
-  
-  pred_bagtree = as.numeric(prob_bagtree > cutoff_bagtree) #ERROR- minimized by maximizing ACCURACY using its cutoff
-  er_cart = mean(pred_bagtree != val_data$target)
-},TRUE)
+summary(perf_bagtree_train)
+summary(perf_bagtree_test)
 
-#er_cart = mean(abs(prob_bagtree - val_data$target)) - probability error 
-
-###RANDOM FOREST ##DONE
-set.seed(87)
-rf = randomForest(as.factor(target)~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
-                  +exang+oldpeak+slope+ca+thal, nodesize = 1 ,data = train_data, proximity = TRUE)
-
-prob_rf_train = predict(rf, train_data, type='prob') # returns probabilities not 0,1 values with type="prob"
-prob_rf_test = predict(rf, val_data, type='prob')
-
-perf_rf_train = performanceMeasures(prob_rf_train[,2], train_data$target, name="RANDOM FOREST Training")
-perf_rf_train
-perf_rf_test = performanceMeasures(prob_rf_test[,2], val_data$target, name="RANDOM FOREST Testing")
-perf_rf_test
 
 ###XGBOOST### ##DONE
 sparse_matrix = sparse.model.matrix(target ~ ., data = train_data)[,-1] #dummy contrast coding of categorical variables to fit the xgboost
@@ -296,72 +382,6 @@ er_nn = mean(nnetpred$data$response != nnetpred$data$truth)
 auc = generateThreshVsPerfData(nnetpred, measures = list(fpr, tpr, mmce))
 plotROCCurves(auc)
 auc_nn = mlr::performance(nnetpred, mlr::auc)
-
-
-#######################
-####Replicated Paper###
-#######################
-
-#### creating 2 functions which we will use ###
-opt.cut = function(perf, pred){ # function which finds optimal cutoff level maximizing a trade-off between Sensitivity& Specifivity
-  cut.ind = mapply(FUN=function(x, y, p){
-    d = (x - 0)^2 + (y-1)^2
-    ind = which(d == min(d))
-    c(sensitivity = y[[ind]], specificity = 1-x[[ind]], 
-      cutoff = p[[ind]])
-  }, perf@x.values, perf@y.values, pred@cutoffs)
-}
-performanceMeasures<-function(prob, truth, name="model") { #Function for various accuracy measures we use.
-  pred= ROCR::prediction(prob, truth)
-  roc = ROCR::performance(pred, measure = "tpr", x.measure = "fpr") #performance measure as ROC
-  auc.perf= ROCR::performance(pred, measure = "auc") #performance measure AUC
-  auc= auc.perf@y.values[[1]]
-  sen_spe = opt.cut(roc, pred)
-  acc.perf = ROCR::performance(pred, measure = "acc") # find best cutoff according to accuracy
-  ind = which.max( slot(acc.perf, "y.values")[[1]] )
-  acc = slot(acc.perf, "y.values")[[1]][ind]
-  cutoff= slot(acc.perf, "x.values")[[1]][ind]
-  pred_cut = as.numeric(prob > cutoff) #ERROR- minimized by maximizing ACCURACY using its cutoff
-  er = mean(pred_cut != truth)
-  data.frame(model=name, Area_under_the_curve = auc, Sensitivity = sen_spe[1], 
-             Specificity= sen_spe[2], Cutoff_Sen_Spe= sen_spe[3], Accuracy=acc, Cutoff_acc = cutoff, error=er)
-}
-
-###LOGISTIC REGRESSION### ##DONE
-logit = glm(fmla, data = train_data, family = "binomial")
-
-prob_log_train = predict(logit, train_data, type = "response")
-prob_log_test = predict(logit, val_data, type = "response")
-
-perf_log_train = performanceMeasures(prob_log_train, train_data$target, name="Logit Training")
-perf_log_train
-perf_log_test = performanceMeasures(prob_log_test, val_data$target, name="Logit Testing")
-dim(perf_log_test)
-
-###DECISION TREES### ##DONE
-cart = rpart(fmla, data = train_data, method = 'class')
-rpart.plot(cart)
-
-prob_cart_train = predict(cart, train_data)
-prob_cart_test = predict(cart, val_data) #prediction
-
-perf_cart_train = performanceMeasures(prob_cart_train[,2], train_data$target, name="DECISION TREES Training")
-perf_cart_train
-perf_cart_test = performanceMeasures(prob_cart_test[,2], val_data$target, name="DECISION TREES Testing")
-perf_cart_test
-
-###NEURAL NETWORKS### ##DONE
-set.seed(69)
-nn = nnet(target~ï..age+sex+cp+trestbps+chol+fbs+restecg+thalach
-          +exang+oldpeak+slope+ca+thal, data = train_data, size = 5, decay = 5e-4, maxit = 100)
-
-prob_nn_train = predict(nn, train_data)
-prob_nn_test = predict(nn, val_data) #prediction
-
-perf_nn_train = performanceMeasures(prob_nn_train, train_data$target, name="NEURAL NETWORKS Training")
-perf_nn_train
-perf_nn_test = performanceMeasures(prob_nn_test, val_data$target, name="NEURAL NETWORKS Testing")
-perf_nn_test
 
 ################
 ###STATISTICS###
